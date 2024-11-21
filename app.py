e3dfb77e419b00b1e3732d10ca27547283eeab98
camerongardner <gar21019@byui.edu>
2024-11-21 14:38:59 -0700

    Updated the requirements.txt file to include module versions.

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ app.register_blueprint(main_blueprint)
 
 # Run the app if executed directly
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
